b1e3cb4bfa5bb84e07cf3a1d6cbd80609bf906ac
Mariusz-72 <mmbwdap@gmail.com>
2024-01-03 18:01:58 +0100

    update 4.1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import contactsReducer from './contacts';
-// import filtersReducer from './filters';
-
-// const store = configureStore({
-//   reducer: {
-//     contacts: contactsReducer,
-//     filters: filtersReducer,
-//   },
-// });
-
-// export default store;
 import { configureStore } from '@reduxjs/toolkit';
 
 import { rootReducer } from './root-reduser';
