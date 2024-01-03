00ffbfc5e29c0a48bf92a186bdd0ed0898f95d18
Mariusz-72 <mmbwdap@gmail.com>
2024-01-03 21:45:45 +0100

    update 4.4

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
-//import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom/client';
+import { App } from '../src/components/App.jsx';
+import './index.css';
 
 import { Provider } from 'react-redux';
-import store from './src/Redux/Store';
 
-import App from './components/App.jsx';
-
-//const root = createRoot(document.getElementById('root'));
+import { store } from './Redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    {/*<React.StrictMode>*/}
+    <React.StrictMode> 
     <App />
-    {/*<React.StrictMode>*/}
+     </React.StrictMode> 
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-
-import { rootReducer } from './root-reduser';
+import { rootReducer } from './root-reducer';
 
 export const store = configureStore({
   reducer: rootReducer,
