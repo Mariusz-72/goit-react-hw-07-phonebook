47772388e4b824e56a4ebeeaeb177bea23db3652
Mariusz-72 <mmbwdap@gmail.com>
2024-01-03 22:49:33 +0100

    update 4.5

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './root-reducer';
+import { rootReducer } from '../Redux/root-reducer';
 
 export const store = configureStore({
   reducer: rootReducer,
