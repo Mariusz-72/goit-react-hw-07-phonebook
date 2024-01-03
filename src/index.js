792ee095304be5bc03abf0fdd6c4967d45d021c5
Mariusz-72 <mmbwdap@gmail.com>
2024-01-03 21:50:42 +0100

    update 4.4.1

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import './index.css';
 
 import { Provider } from 'react-redux';
 
-import { store } from './Redux/store.js';
+import { store } from './Redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode> 
     <App />
-     </React.StrictMode> 
+      </React.StrictMode> 
   </Provider>
 );
